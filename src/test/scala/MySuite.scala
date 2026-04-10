12708d26cf6e92053940588cee0ef02046dd672d
Lord-Juice <kruesselpaul@gmail.com>
2026-04-10 11:24:00 +0200

    Added basic test to test string output of the main function

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,21 +1,59 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+class MainTests extends munit.FunSuite {
+  test("titleRow should contain CUBATRO") {
+    assert(titleRow.contains("CUBATRO"))
   }
-  test("show off code completion") {
-    val xs = List(1, 2, 3, 4, 5)
-    val (even, odd) = xs.partition(_ % 2 == 0)
-    assertEquals(even.size, 2)
-    assertEquals(odd.size, 3)
 
-    xs match
-      case first :: second :: next => println("2 or more elements")
-      case first :: Nil => println("1 element")
-      case Nil => println("Empty")
-    
+  test("titleRow should have borders") {
+    assert(titleRow.contains("+-----"))
+  }
+
+  test("scoreBoard should contain Level Score") {
+    assert(scoreBoard.contains("Level Score"))
+  }
+
+  test("scoreBoard should contain Current Score") {
+    assert(scoreBoard.contains("Current Score"))
+  }
+
+  test("upgradeBoard should contain Cupgrades") {
+    assert(upgradeBoard.contains("Cupgrades"))
+  }
+
+  test("upgradeBoard should list upgrades") {
+    assert(upgradeBoard.contains("Leather"))
+    assert(upgradeBoard.contains("Steel"))
+    assert(upgradeBoard.contains("Pocket Dimension"))
+    assert(upgradeBoard.contains("Blue Sticker"))
+  }
+
+  test("lockedRows should contain Locked Rows") {
+    assert(lockedRows.contains("Locked Rows"))
+  }
+
+  test("draftScore should contain Chips x Mult") {
+    assert(draftScore.contains("Chips x Mult"))
+  }
+
+  test("draftRow should contain Draft Row") {
+    assert(draftRow.contains("Draft Row"))
+  }
+
+  test("rolledDice should contain Rolled Dice") {
+    assert(rolledDice.contains("Rolled Dice"))
+  }
+
+  test("diceHand should contain Hand") {
+    assert(diceHand.contains("Hand"))
+  }
+
+  test("all functions should return non-empty strings") {
+    assert(titleRow.nonEmpty)
+    assert(scoreBoard.nonEmpty)
+    assert(upgradeBoard.nonEmpty)
+    assert(lockedRows.nonEmpty)
+    assert(draftScore.nonEmpty)
+    assert(draftRow.nonEmpty)
+    assert(rolledDice.nonEmpty)
+    assert(diceHand.nonEmpty)
   }
 }
