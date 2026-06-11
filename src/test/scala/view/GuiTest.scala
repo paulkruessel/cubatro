12df31bd35d7563d35a6da43ce6b19d5510efb3a
paulkruessel <kruesselpaul@gmail.com>
2026-06-11 21:05:08 +0200

    Add initial GuiTest class for GUI component testing

---
+++ src/test/scala/view/GuiTest.scala
@@ -0,0 +1,13 @@
+package view
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import controller.*
+import model.*
+import view.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class GuiTest extends AnyWordSpec with Matchers {
+
+}
