447181a8204176c29a36b971968e5cda8870bfce
Lord-Juice <kruesselpaul@gmail.com>
2026-06-11 11:14:20 +0200

    Added a check for a graphical interface for the gui

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,24 @@
 import controller.GameController
 import view.{Gui, Tui}
 
-import scala.concurrent.Future
+import java.awt.GraphicsEnvironment
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.swing.Swing
 
 @main def main(): Unit =
   val controller = new GameController()
   val tui = new Tui(controller)
 
-  Swing.onEDT {
-    val gui = new Gui(controller)
-    gui.visible = true
-  }
-
   controller.start()
+
+  if !GraphicsEnvironment.isHeadless() then
+    Swing.onEDT {
+      val gui = new Gui(controller)
+      gui.visible = true
+    }
+  else
+    println("GUI not started: no graphical display available.")
 
   Future {
     tui.run()
